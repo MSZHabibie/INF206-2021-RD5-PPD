99f65efbf2cfb357be0768fa55f8b08f1ecb7154
Siti Mawaddah <siti.wadd@mhs.unsyiah.ac.id>
2021-05-08 00:22:33 +0700

    halaman dashboard admin

--- app/controllers/Dashboard.php
+++ app/controllers/Dashboard.php
@@ -6,10 +6,22 @@ class Dashboard extends Controller
     public function index()
     {
         $this->hasSession();
+        $this->isAdmin(get_class($this));
 
         $data['judul'] = 'Dashboard';
         $this->view('templates/header', $data);
         $this->view('dashboard/index');
+        $this->view('templates/footer');
+    }
+
+    public function admin()
+    {
+        $this->hasSession();
+        $this->isNotAdmin(get_class($this));
+
+        $data['judul'] = 'Dashboard Admin';
+        $this->view('templates/header', $data);
+        $this->view('dashboard/admin');
         $this->view('templates/footer');
     }
 }
