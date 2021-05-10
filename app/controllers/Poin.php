6bb0a1fabf8fdc0b2e43c8808dfeec202645f64d
Muhammad Syah Zichrullah Habibie <mszhabibie@gmail.com>
2021-05-10 21:44:22 +0700

    Membuat fungsi index dan admin untuk poin
    Yang membedakannya adalah pada index hanya ditampilkan poin untuk 1 user dan menampilkan semua voucher. sedangkan pada admin akan menampilkan poin semua user dan semua voucher

--- app/controllers/Poin.php
+++ app/controllers/Poin.php
@@ -2,11 +2,29 @@
 
 class Poin extends Controller
 {
-  public function index()
+  public function index($id)
   {
+    $this->hasSession();
+    $this->isAdmin(get_class($this));
+
     $data['judul'] = 'Poin';
+    $data['poin'] = $this->model('Poin_model')->getPoinId($id);
+    $data['voucher'] = $this->model('Poin_model')->getAllVoucher();
     $this->view('templates/header', $data);
-    $this->view('Poin/index');
+    $this->view('Poin/index', $data);
+    $this->view('templates/footer');
+  }
+  public function admin()
+  {
+    $this->hasSession();
+    $this->isNotAdmin(get_class($this));
+
+    $data['judul'] = 'Poin Admin';
+    $data['poin'] = $this->model('Poin_model')->getAllPoin();
+    $data['voucher'] = $this->model('Poin_model')->getAllVoucher();
+    $data['admin'] = $_SESSION['admin'];
+    $this->view('templates/header', $data);
+    $this->view('poin/admin', $data);
     $this->view('templates/footer');
   }
 }
