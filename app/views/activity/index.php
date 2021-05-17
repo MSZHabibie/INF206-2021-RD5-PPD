7f000beb76aed12a8d85fbbb0912820c3a467abe
Siti Mawaddah <siti.wadd@mhs.unsyiah.ac.id>
2021-05-18 00:07:51 +0700

    card activity

--- app/views/activity/index.php
+++ app/views/activity/index.php
@@ -130,6 +130,32 @@
 					</h1>
 				</section>
 
+				<table width="300">
+					<tr>
+						<td>
+							<img width="300px" height="200px" src="https://www.denpasarkota.go.id/uploads/berita/Sambut_Bulan_Bakti_Gotong_Royong_306847.jpg" alt="Gotong Royong">
+						</td>
+						<td>
+							<img width="300px" height="200px" src="https://infopublik.id/assets/upload/headline//Senam.JPG" alt="Senam Pagi">
+						</td>
+						<td>
+							<img width="300px" height="200px" src="https://img.gesuri.id/img/content/2019/11/22/54716/cegah-banjir-pemkab-brebes-galakkan-penghijauan-YP2nG8ZmOk.jpg" alt="Penghijauan">
+						</td>
+					</tr>
+
+					<tr align="center">
+						<td><b>Gotong Royong</b></td>
+						<td><b>Senam Pagi</b></td>
+						<td><b>Penghijauan</b></td>
+					</tr>
+
+					<tr>
+						<td><b>Gotong royong</b> adalah kegiatan yang dilakukan dengan bekerja sama untuk mencapai hasil yang diinginkan</td>
+						<td><b>Senam pagi</b> adalah suatu aktifitas yang dilakukan pada pagi hari untuk menjaga kebugaran jasmani</td>
+						<td><b>Penghijauan</b> adalah salah satu bentuk peran dalam menjaga lingkungan dengan melakukan penanaman pepohonan</td>
+					</tr>
+				</table>
+
 				<div class="row row-cols-1 row-cols-md-3 g-4">
 					<?php foreach ($data['aktivitas'] as $aktivitas) : ?>
 						<div class="col mb-3">
