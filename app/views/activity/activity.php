a3a49eefcd79b81d2b0359a1a38101f6bd5ea5b3
Siti Mawaddah <siti.wadd@mhs.unsyiah.ac.id>
2021-05-17 19:16:21 +0700

    menambah tombol detail di activity

--- app/views/activity/activity.php
+++ app/views/activity/activity.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta charset="UTF-8">
 	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" name="viewport">
@@ -13,8 +14,9 @@
 	<link rel="stylesheet" href="<?= BASEURL; ?>/modules/flag-icon-css/css/flag-icon.min.css">
 	<link rel="stylesheet" href="<?= BASEURL; ?>/css/CSS_yovie/css/demo.css">
 	<link rel="stylesheet" href="<?= BASEURL; ?>/css/CSS_yovie/css/style.css">
-	
+
 </head>
+
 <body>
 	<div id="app">
 		<div class="main-wrapper">
@@ -67,7 +69,7 @@
 					</li>
 					<li class="dropdown">
 						<a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg"> <i class="ion ion-android-person d-lg-none"></i>
-							<div class="d-sm-none d-lg-inline-block">Hi, Yovie Pramudya Sabri</div>
+							<div class="d-sm-none d-lg-inline-block">Hi, <?= $data['warga']['username']; ?></div>
 						</a>
 						<div class="dropdown-menu dropdown-menu-right">
 							<a href="#" class="dropdown-item has-icon"> <i class="ion ion-android-person"></i> Profile </a>
@@ -82,20 +84,20 @@
 					<div class="sidebar-user">
 						<div class="sidebar-user-picture"> <img alt="image" src="<?= BASEURL; ?>/img/img_yovie/img/avatar/avatar-5.jpeg"> </div>
 						<div class="sidebar-user-details">
-							<div class="user-name">Yovie Pramudya Sabri</div>
-							<div class="user-role"> Administrator </div>
+							<div class="user-name"><?= $data['warga']['username']; ?></div>
+							<div class="user-role"> Warga </div>
 						</div>
 					</div>
 					<ul class="sidebar-menu">
 						<li class="menu-header">Dashboard</li>
-						<li > <a href="<?= BASEURL; ?>/dashboard/index"><i class="ion ion-speedometer"></i><span>Dashboard</span></a> </li>
+						<li> <a href="<?= BASEURL; ?>/dashboard/index"><i class="ion ion-speedometer"></i><span>Dashboard</span></a> </li>
 						<li class="menu-header">Profile</li>
-						<li> <a href="#" ><i class="fas fa-users"></i><span>Profile</span></a></li>
+						<li> <a href="#"><i class="fas fa-users"></i><span>Profile</span></a></li>
 						<li class="menu-header">Activity</li>
-						<li class="active"> <a href="<?= BASEURL; ?>/activity/index" ><i class="fas fa-american-sign-language-interpreting"></i><span>Activity</span></a></li>
-						<li> <a href="#" ><i class="fas fa-burn"></i><span>Points</span></a></li>
+						<li class="active"> <a href="<?= BASEURL; ?>/activity/index"><i class="fas fa-american-sign-language-interpreting"></i><span>Activity</span></a></li>
+						<li> <a href="#"><i class="fas fa-burn"></i><span>Points</span></a></li>
 						<li> <a href="table.html"><i class="far fa-address-card"></i><span>Community</span></a> </li>
-						
+
 						<li class="menu-header">More</li>
 						<li> <a href="#" class="has-dropdown"><i class="ion ion-ios-nutrition"></i> About as</a>
 							<ul class="menu-dropdown">
@@ -124,90 +126,49 @@
 			<div class="main-content">
 				<section class="section">
 					<h1 class="section-header">
-            <div>Activity</div>
-        </h1>
-        	<table width="300">
-        		<tr>
-        			<td>
-        				<img width="300px" height="200px" src="https://www.denpasarkota.go.id/uploads/berita/Sambut_Bulan_Bakti_Gotong_Royong_306847.jpg" alt="Gotong Royong">
-        			</td>
-        			<td>
-        				<img width="300px" height="200px" src="https://infopublik.id/assets/upload/headline//Senam.JPG" alt="Senam Pagi">
-        			</td>
-        			<td>
-        				<img width="300px" height="200px" src="https://img.gesuri.id/img/content/2019/11/22/54716/cegah-banjir-pemkab-brebes-galakkan-penghijauan-YP2nG8ZmOk.jpg" alt="Penghijauan">
-        			</td>
-        		</tr>
+						<div>Activity</div>
+					</h1>
+				</section>
 
-        		<tr align="center">
-        			<td><b>Gotong Royong</b></td>
-        			<td><b>Senam Pagi</b></td>
-        			<td><b>Penghijauan</b></td>
-        		</tr>
+				<div class="row row-cols-1 row-cols-md-3 g-4">
+					<?php foreach ($data['aktivitas'] as $aktivitas) : ?>
+						<div class="col mb-3">
+							<div class="card h-100" style="width: 18rem;">
+								<img src="<?= BASEURL; ?>/img/<?= $aktivitas['gambar'] ?>" class="card-img-top" alt="">
+								<div class="card-body">
+									<h5 class="card-title"><?= $aktivitas['nama'] ?></h5>
+									<p class="card-text"><?= $aktivitas['deskripsi'] ?></p>
+								</div>
+								<div class="card-footer">
+									<a href="<?= BASEURL; ?>/activity/detail/<?= $aktivitas['id'] ?>" class="btn btn-primary">Detail</a>
+								</div>
+							</div>
+						</div>
+					<?php endforeach; ?>
+				</div>
 
-        		<tr>
-        			<td><b>Gotong royong</b> adalah kegiatan yang dilakukan dengan bekerja sama untuk mencapai hasil yang diinginkan</td>
-        			<td><b>Senam pagi</b> adalah suatu aktifitas yang dilakukan pada pagi hari untuk menjaga kebugaran jasmani</td>
-        			<td><b>Penghijauan</b> adalah salah satu bentuk peran dalam menjaga lingkungan dengan melakukan penanaman pepohonan</td>
-        		</tr>        		
-        	</table>
-
-			<footer class="main-footer">
-				<div class="footer-left"> Copyright &copy; RD 5 2021
-					<div class="bullet"></div> Design By <a href="#">RD 5</a> </div>
-				<div class="footer-right"></div>
-			</footer>
+				<footer class="main-footer">
+					<div class="footer-left"> Copyright &copy; RD 5 2021
+						<div class="bullet"></div> Design By <a href="#">RD 5</a>
+					</div>
+					<div class="footer-right"></div>
+				</footer>
+			</div>
 		</div>
-	</div>
-	<script src="<?= BASEURL; ?>/modules/jquery.min.js"></script>
-	<script src="<?= BASEURL; ?>/modules/popper.js"></script>
-	<script src="<?= BASEURL; ?>/modules/tooltip.js"></script>
-	<script src="<?= BASEURL; ?>/modules/bootstrap/js/bootstrap.min.js"></script>
-	<script src="<?= BASEURL; ?>/modules/nicescroll/jquery.nicescroll.min.js"></script>
-	<script src="<?= BASEURL; ?>/modules/scroll-up-bar/dist/scroll-up-bar.min.js"></script>
-	<script src="<?= BASEURL; ?>/js/js_yovie/js/sa-functions.js"></script>
-	<script src="<?= BASEURL; ?>/modules/chart.min.js"></script>
-	<script src="<?= BASEURL; ?>/modules/summernote/summernote-lite.js"></script>
-	<script>
-	var ctx = document.getElementById("myChart").getContext('2d');
-	var myChart = new Chart(ctx, {
-		type: 'line',
-		data: {
-			labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-			datasets: [{
-				label: 'Statistics',
-				data: [460, 458, 330, 502, 430, 610, 488],
-				borderWidth: 2,
-				backgroundColor: 'rgb(87,75,144)',
-				borderColor: 'rgb(87,75,144)',
-				borderWidth: 2.5,
-				pointBackgroundColor: '#ffffff',
-				pointRadius: 4
-			}]
-		},
-		options: {
-			legend: {
-				display: false
-			},
-			scales: {
-				yAxes: [{
-					ticks: {
-						beginAtZero: true,
-						stepSize: 150
-					}
-				}],
-				xAxes: [{
-					gridLines: {
-						display: false
-					}
-				}]
-			},
-		}
-	});
-	</script>
-	<script src="https://kit.fontawesome.com/9672737e03.js" crossorigin="anonymous"></script>
-	<script src="<?= BASEURL; ?>/js/js_yovie/js/scripts.js"></script>
-	<script src="<?= BASEURL; ?>/js/js_yovie/js/custom.js"></script>
-	<script src="<?= BASEURL; ?>/js/js_yovie/js/demo.js"></script>
+
+		<script src="<?= BASEURL; ?>/modules/jquery.min.js"></script>
+		<script src="<?= BASEURL; ?>/modules/popper.js"></script>
+		<script src="<?= BASEURL; ?>/modules/tooltip.js"></script>
+		<script src="<?= BASEURL; ?>/modules/bootstrap/js/bootstrap.min.js"></script>
+		<script src="<?= BASEURL; ?>/modules/nicescroll/jquery.nicescroll.min.js"></script>
+		<script src="<?= BASEURL; ?>/modules/scroll-up-bar/dist/scroll-up-bar.min.js"></script>
+		<script src="<?= BASEURL; ?>/js/js_yovie/js/sa-functions.js"></script>
+		<script src="<?= BASEURL; ?>/modules/chart.min.js"></script>
+		<script src="<?= BASEURL; ?>/modules/summernote/summernote-lite.js"></script>
+		<script src="https://kit.fontawesome.com/9672737e03.js" crossorigin="anonymous"></script>
+		<script src="<?= BASEURL; ?>/js/js_yovie/js/scripts.js"></script>
+		<script src="<?= BASEURL; ?>/js/js_yovie/js/custom.js"></script>
+		<script src="<?= BASEURL; ?>/js/js_yovie/js/demo.js"></script>
 </body>
+
 </html>
