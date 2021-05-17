ba74bd2bf4c36afe95415b77d129811f06b18456
Fiya Yufita <fiyayunita@mhs.unsyiah.ac.id>
2021-05-17 22:18:58 +0700

    Pengubahan pada file community.php

--- app/controllers/Community.php
+++ app/controllers/Community.php
@@ -1,16 +1,16 @@
-<?php 
+<?php
 
 class Community extends Controller
 {
-    public function index()
-    {
-        $this->hasSession();
-        $this->isAdmin(get_class($this));
-        
-        $data['judul'] = 'Daftar Komunitas';
-        $data['com'] = $this->model('Komunitas_model')->getAllCommunity();
-        $this->view('templates/header', $data);
-        $this->view('community/index', $data);
-        $this->view('templates/footer');
-    }
+  public function index()
+  { 
+    $data['judul'] = 'Community';
+    $data['komunitas'] = $this->model('Community_model')->getAllCommunity();
+    $this->view('templates/header', $data);
+    $this->view('community/index', $data);
+    $this->view('templates/footer');
+  }
+
 }
+
+
