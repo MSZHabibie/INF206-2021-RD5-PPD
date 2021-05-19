89b677567df275fb00da26c87eca936d514d13a9
Siti Mawaddah <siti.wadd@mhs.unsyiah.ac.id>
2021-05-19 22:26:36 +0700

    link halaman di sidebar

--- app/controllers/Community.php
+++ app/controllers/Community.php
@@ -1,18 +1,17 @@
 <?php
+session_start();
 
 class Community extends Controller
 {
   public function index()
-  { 
+  {
     $this->hasSession();
     $this->isAdmin(get_class($this));
-  
+
     $data['judul'] = 'Community';
     $data['komunitas'] = $this->model('Community_model')->getAllCommunity();
     $this->view('templates/header', $data);
     $this->view('community/index', $data);
     $this->view('templates/footer');
   }
-
 }
-
