e83fb572993b9e35cd79abb7b7876a002686a6fb
Siti Mawaddah <siti.wadd@mhs.unsyiah.ac.id>
2021-05-19 20:44:21 +0700

    menghubungkan tampilan detail activity

--- app/views/activity/detail.php
+++ app/views/activity/detail.php
@@ -1,28 +1,51 @@
-<div class="container mt-5">
+<div class="main-content">
+    <section class="section">
+        <h1 class="section-header">
+            <div>Activity</div>
+        </h1>
+        <table border="1" align="center" width="300">
+            <tr>
+                <td>
+                    <img width="300px" height="200px" src="<?= BASEURL; ?>/img/activity/<?= $data['aktivitas']['gambar'] ?>" alt="<?= $data['aktivitas']['nama'] ?>">
+                </td>
+            </tr>
 
-    <div class="card" style="width: 18rem;">
-        <img src="<?= BASEURL; ?>/img/activity/<?= $data['aktivitas']['gambar'] ?>" class="card-img-top" alt="">
-        <div class="card-body">
-            <h5 class="card-title"><?= $data['aktivitas']['nama'] ?></h5>
-        </div>
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">Persyaratan: <?= $data['aktivitas']['syarat'] ?></li>
-            <li class="list-group-item">Peserta: <?= $data['aktivitas']['peserta'] ?>/<?= $data['aktivitas']['maks_peserta'] ?></li>
-            <li class="list-group-item">Waktu: <?= $data['aktivitas']['waktu'] ?></li>
-            <li class="list-group-item">Tempat: <?= $data['aktivitas']['tempat'] ?></li>
-            <li class="list-group-item">Point: <?= $data['aktivitas']['poin'] ?></li>
-            <li class="list-group-item">Deskripsi:
-                <p class="card-text"><?= $data['aktivitas']['deskripsi'] ?></p>
-            </li>
-        </ul>
-        <div class="card-body">
-            <form action="<?= BASEURL; ?>/activity/daftar/<?= $data['warga']['id']; ?>/<?= $data['aktivitas']['id']; ?>" method="post">
-                <a href="<?= BASEURL; ?>/activity" class="btn btn-primary">Kembali</a>
-                <button type="submit" class="btn btn-primary" name="<?= !$data['aktivitas_warga'] ? 'daftar' : 'batal_daftar'; ?>" <?= $data['aktivitas']['peserta'] == $data['aktivitas']['maks_peserta'] ? 'disabled' : ''; ?>><?= !$data['aktivitas_warga'] ? 'Daftar' : 'Batal Daftar'; ?></button>
+            <tr align="center">
+                <td><b><?= $data['aktivitas']['nama'] ?></b></td>
+            </tr>
 
-            </form>
+            <tr>
+                <td>Persyaratan: <?= $data['aktivitas']['syarat'] ?></td>
+            </tr>
 
-        </div>
-    </div>
+            <tr>
+                <td>Peserta: <?= $data['aktivitas']['peserta'] ?>/<?= $data['aktivitas']['maks_peserta'] ?></td>
+            </tr>
 
-</div>
+            <tr>
+                <td>Waktu: <?= $data['aktivitas']['waktu'] ?></td>
+            </tr>
+
+            <tr>
+                <td>Tempat: <?= $data['aktivitas']['tempat'] ?></td>
+            </tr>
+
+            <tr>
+                <td>Point: <?= $data['aktivitas']['poin'] ?></td>
+            </tr>
+
+            <tr>
+                <td><b>Deskripsi:</b><br><?= $data['aktivitas']['deskripsi'] ?></br></td>
+            </tr>
+
+            <tr>
+                <td>
+                    <div class="text-center">
+                        <form action="<?= BASEURL; ?>/activity/daftar/<?= $data['warga']['id']; ?>/<?= $data['aktivitas']['id']; ?>" method="post">
+                            <a href="<?= BASEURL; ?>/activity" class="btn btn-primary btn-round">Kembali</a>
+                            <button type="submit" class="btn btn-primary btn-round" name="<?= !$data['aktivitas_warga'] ? 'daftar' : 'batal_daftar'; ?>" <?= $data['aktivitas']['peserta'] == $data['aktivitas']['maks_peserta'] ? 'disabled' : ''; ?>><?= !$data['aktivitas_warga'] ? 'Daftar' : 'Batal Daftar'; ?></button>
+                        </form>
+                    </div>
+                </td>
+            </tr>
+        </table>
