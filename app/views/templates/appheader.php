df5223f12d06214249eb7a998302ffb851570358
Siti Mawaddah <siti.wadd@mhs.unsyiah.ac.id>
2021-05-26 15:06:42 +0700

    icon ppd

--- app/views/templates/appheader.php
+++ app/views/templates/appheader.php
@@ -6,7 +6,7 @@
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" name="viewport">
     <title>PPD | <?= $data['judul']; ?></title>
     <!--favicon-->
-    <link rel="icon" href="<?= BASEURL; ?>/img/img_yovie/img/avatar/icones.ico" type="image">
+    <link rel="icon" href="<?= BASEURL; ?>/img/logo.png" type="image">
     <link rel="stylesheet" href="<?= BASEURL; ?>/modules/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?= BASEURL; ?>/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?= BASEURL; ?>/modules/ionicons/css/ionicons.min.css">
