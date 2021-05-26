6530613676f9a31190fdf6f8d5c9c481e13f1407
Siti Mawaddah <siti.wadd@mhs.unsyiah.ac.id>
2021-05-26 22:09:40 +0700

    fitur edit profile

--- app/views/profile/edit.php
+++ app/views/profile/edit.php
@@ -13,73 +13,77 @@
     <section>
         <div class="container">
             <h2>Edit Profile</h2>
-            <div class="row100">
-                <div class="col">
-                    <div class="inputBox">
-                        <input type="text" name="" required="required">
-                        <span class="text">Nama</span>
-                        <span class="line"></span>
+            <form action="<?= BASEURL; ?>/profile/<?= isset($_SESSION['admin']) ? 'updateAdmin' : 'update'; ?>" method="post">
+                <input type="hidden" name="id" id="id" value="<?= $data['user']['id'] ?>">
+                <div class="row100">
+                    <div class="col">
+                        <div class="inputBox">
+                            <input type="text" name="nama" required="required" value="<?= $data['user']['nama'] ?>">
+                            <span class="text">Nama</span>
+                            <span class="line"></span>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="inputBox">
+                            <input type="number" min="0" name="umur" required="required" value="<?= $data['user']['umur'] ?>">
+                            <span class="text">Umur</span>
+                            <span class="line"></span>
+                        </div>
                     </div>
                 </div>
-                <div class="col">
-                    <div class="inputBox">
-                        <input type="text" name="" required="required">
-                        <span class="text">Umur</span>
-                        <span class="line"></span>
-                    </div>
-                </div>
-            </div>
 
-            <div class="row100">
-                <div class="col">
-                    <div class="inputBox">
-                        <input type="text" name="" required="required">
-                        <span class="text">Tempat,tanggal lahir</span>
-                        <span class="line"></span>
+                <div class="row100">
+                    <div class="col">
+                        <div class="inputBox">
+                            <input type="text" name="tempat_lahir" required="required" value="<?= $data['user']['tempat_lahir'] ?>">
+                            <span class="text">Tempat lahir</span>
+                            <span class="line"></span>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="inputBox">
+                            <input type="date" name="tanggal_lahir" value="<?= $data['user']['tanggal_lahir'] ?>">
+                            <span class="text">Tanggal lahir</span>
+                            <span class="line"></span>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="inputBox">
+                            <input type="tel" name="no_hp" required="required" value="<?= $data['user']['no_hp'] ?>">
+                            <span class="text">No HP</span>
+                            <span class="line"></span>
+                        </div>
                     </div>
                 </div>
-                <div class="col">
-                    <div class="inputBox">
-                        <input type="text" name="" required="required">
-                        <span class="text">No HP</span>
-                        <span class="line"></span>
+
+                <div class="row100">
+                    <div class="col">
+                        <div class="inputBox textarea">
+                            <textarea name="bio" required="required"><?= $data['user']['bio'] ?></textarea>
+                            <span class="text">Bio</span>
+                            <span class="line"></span>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="inputBox textarea">
+                            <textarea name="alamat" required="required"><?= $data['user']['alamat'] ?></textarea>
+                            <span class="text">Alamat</span>
+                            <span class="line"></span>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-
-
-            <div class="row100">
-                <div class="col">
-                    <div class="inputBox textarea">
-                        <textarea required="required"></textarea>
-                        <span class="text">Bio</span>
-                        <span class="line"></span>
+                <div class="row100">
+                    <div class="col">
+                        <a href="<?= BASEURL; ?>/profile">Cancel</a>
+                    </div>
+                    <div class="col">
+                        <input type="submit" value="Save">
                     </div>
                 </div>
-            </div>
-            <div class="row100">
-                <div class="col">
-                    <div class="inputBox textarea">
-                        <textarea required="required"></textarea>
-                        <span class="text">Alamat</span>
-                        <span class="line"></span>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row100">
-                <div class="col">
-                    <a href="<?= BASEURL; ?>/profile">Cancel</a>
-                </div>
-                <div class="col">
-                    <input type="submit" value="Save">
-                </div>
-            </div>
+            </form>
         </div>
-
-
-
     </section>
 </body>
 
