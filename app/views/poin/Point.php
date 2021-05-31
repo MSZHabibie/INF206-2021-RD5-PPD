80c7e0b8087c0db553a7d2c23b873cb07610d89b
yovie <yoviesabri@mhs.unsyiah.ac.id>
2021-05-31 21:51:19 +0700

    Perubahan file point

--- app/views/poin/Point.php
+++ app/views/poin/Point.php
@@ -6,13 +6,16 @@
 	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" name="viewport">
 	<title>PPD</title>
 	<!--favicon-->
-	<link rel="icon" href="../dist/img/avatar/favicon.ico" type="image/x-icon">
-	<link rel="stylesheet" href="../dist/modules/bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" href="../dist/modules/ionicons/css/ionicons.min.css">
-	<link rel="stylesheet" href="../dist/modules/fontawesome/web-fonts-with-css/css/fontawesome-all.min.css">
-	<link rel="stylesheet" href="../dist/css/demo.css">
-	<link rel="stylesheet" href="../dist/css/style.css"> 
-	<link rel="stylesheet" href="../dist/css/Point.css">
+	<link rel="icon" href="<?= BASEURL; ?>/img/img_yovie/img/avatar/icones.ico" type="image">
+	<link rel="stylesheet" href="<?= BASEURL; ?>/modules/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?= BASEURL; ?>/modules/ionicons/css/ionicons.min.css">
+	<link rel="stylesheet" href="<?= BASEURL; ?>/modules/fontawesome/web-fonts-with-css/css/fontawesome-all.min.css">
+	<link rel="stylesheet" href="<?= BASEURL; ?>/modules/summernote/summernote-lite.css">
+	<link rel="stylesheet" href="<?= BASEURL; ?>/modules/flag-icon-css/css/flag-icon.min.css">
+	<link rel="stylesheet" href="<?= BASEURL; ?>/css/CSS_yovie/css/demo.css">
+	<link rel="stylesheet" href="<?= BASEURL; ?>/css/CSS_yovie/css/style.css">
+	<link rel="stylesheet" href="<?= BASEURL; ?>/css/CSS_yovie/css/Point.css">
+	
 </head>
 
 <body>
