b428233d17bcc4e4ad985bbe136b1cba65cfbd20
Siti Mawaddah <siti.wadd@mhs.unsyiah.ac.id>
2021-06-01 10:25:00 +0700

    menambah logo di login dan register

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -1,6 +1,9 @@
 <div class="container">
     <div class="form">
-        <h2>LOGIN</h2>
+        <a href="<?= BASEURL; ?>/home" style="margin-top: 0 !important; justify-content: flex-end; display: flex;">
+            <img src="<?= BASEURL; ?>/img/logo.png" alt="Pensejahteraan Pemuda Desa" width="20%">
+        </a>
+        <h2 style="font-size: calc(1.375rem + 1.5vw);font-weight: 300;line-height: 1.2;">LOGIN</h2>
         <?php if (isset($_POST['submit'])) : ?>
             <div class="alert alert-danger" role="alert">
                 <?= $data['gagallogin'] ?>
@@ -16,7 +19,6 @@
             <div class="inputBox">
                 <input type="submit" value="Sign In" name="submit">
             </div>
-            <p class="forget">Forgot password ? <a href="#" target="_self">Click Here</a></p>
             <p class="forget">Don't have an account ? <a href="<?= BASEURL; ?>/register" target="_self">Sign Up</a></p>
         </form>
     </div>
--- app/views/register/index.php
+++ app/views/register/index.php
@@ -1,6 +1,9 @@
 <div class="container">
     <div class="form">
-        <h2>REGISTER</h2>
+        <a href="<?= BASEURL; ?>/home" style="margin-top: 0 !important; justify-content: flex-end; display: flex;">
+            <img src="<?= BASEURL; ?>/img/logo.png" alt="Pensejahteraan Pemuda Desa" width="20%">
+        </a>
+        <h2 style="font-size: calc(1.250rem + 1.5vw);font-weight: 300;line-height: 0.7;">REGISTER</h2>
         <?php if (isset($_POST['submit'])) : ?>
             <div class="alert alert-danger" role="alert">
                 <?= $data['gagalregister'] ?>
