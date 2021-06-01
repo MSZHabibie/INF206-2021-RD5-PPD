d2410f6f8008e904fca105e71579c57f10df3efb
Fiya Yufita <fiyayunita@mhs.unsyiah.ac.id>
2021-06-01 15:20:29 +0700

    menghubungkan halaman community

--- app/controllers/Community.php
+++ app/controllers/Community.php
@@ -5,13 +5,17 @@ class Community extends Controller
 {
   public function index()
   {
+
     $this->hasSession();
     $this->isAdmin(get_class($this));
 
+    $data['class'] = get_class($this);
     $data['judul'] = 'Community';
+    $data['warga'] = $_SESSION['warga'];
+    $data['user'] = $_SESSION['warga'];
     $data['komunitas'] = $this->model('Community_model')->getAllCommunity();
-    $this->view('templates/header', $data);
+    $this->view('templates/appheader', $data);
     $this->view('community/index', $data);
-    $this->view('templates/footer');
+    $this->view('templates/appfooter');
   }
 }
