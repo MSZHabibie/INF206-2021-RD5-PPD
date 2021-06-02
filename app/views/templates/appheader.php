1c713c10c0bc8783525261835eaf734d26e1461e
Siti Mawaddah <siti.wadd@mhs.unsyiah.ac.id>
2021-06-02 14:20:22 +0700

    fitur confirm warga jika sudah selesai aktivitas

--- app/views/templates/appheader.php
+++ app/views/templates/appheader.php
@@ -23,16 +23,16 @@
         <div class="main-wrapper">
             <div class="navbar-bg"></div>
             <nav class="navbar navbar-expand-lg main-navbar d-flex justify-content-end">
-                <!-- <form class="form-inline mr-auto">
+                <form class="form-inline mr-auto">
                     <ul class="navbar-nav mr-3">
                         <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="ion ion-navicon-round"></i></a></li>
                         <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i class="ion ion-search"></i></a></li>
                     </ul>
-                    <div class="search-element">
+                    <!-- <div class="search-element">
                         <input class="form-control" type="search" placeholder="Search" aria-label="Search">
                         <button class="btn" type="submit"><i class="ion ion-search"></i></button>
-                    </div>
-                </form> -->
+                    </div> -->
+                </form>
                 <ul class="navbar-nav navbar-right">
                     <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown" class="nav-link notification-toggle nav-link-lg beep"><i class="ion ion-ios-bell-outline"></i></a>
                         <div class="dropdown-menu dropdown-list dropdown-menu-right">
